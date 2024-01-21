2ec4161d7953afd4729fa05b188644fea23ec7b6
aiyion.prime <git@aiyionpri.me>
2024-01-21 19:05:49 +0100

    fix: Replace invalid str.decode calls

--- examples/parse.py
+++ examples/parse.py
@@ -3,7 +3,7 @@ import ndef
 
 print('parsing valid ndef')
 
-message_data = 'D1010F5402656E48656C6C6F20776F726C6421'.decode('hex')
+message_data = bytes.fromhex('D1010F5402656E48656C6C6F20776F726C6421')
 message = ndef.NdefMessage(message_data)
 record = message.records[0]
 
@@ -15,7 +15,7 @@ print('  payload:', record.payload)
 print('')
 print('parsing invalid ndef')
 
-message_data = '9901050155610123456761'.decode('hex')
+message_data = bytes.fromhex('9901050155610123456761')
 try:
     message = ndef.NdefMessage(message_data)
     assert False, 'this should never happen'
