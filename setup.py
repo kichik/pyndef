641fdfb79f2f811df0c0f0ccd518a16c388f08ce
aiyion.prime <git@aiyionpri.me>
2024-01-21 18:59:36 +0100

    build: Drop support for EOL python versions
    Since 27th June 2023 Python 3.7 does not receive security updates anymore.

--- setup.py
+++ setup.py
@@ -27,11 +27,6 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: zlib/libpng License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
 )
