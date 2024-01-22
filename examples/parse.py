a922bfbd19a6d9ae593e9251226eeef7235bb338
Jan-Niklas Burfeind <github@aiyionpri.me>
2024-01-22 20:02:55 +0100

    feat: Update to more recent Python versions (#2)
    This one resolves #1 and contains minor necessary changes to the example which would be invalid without.
    
    ---------
    
    Co-authored-by: Amir Szekely <kichik@gmail.com>

--- examples/parse.py
+++ examples/parse.py
@@ -1,23 +1,23 @@
 import ndef
 
 
-print 'parsing valid ndef'
+print('parsing valid ndef')
 
-message_data = 'D1010F5402656E48656C6C6F20776F726C6421'.decode('hex')
+message_data = bytes.fromhex('D1010F5402656E48656C6C6F20776F726C6421')
 message = ndef.NdefMessage(message_data)
 record = message.records[0]
 
-print '  tnf:    ', record.tnf
-print '  type:   ', record.type
-print '  id:     ', record.id
-print '  payload:', record.payload
+print('  tnf:    ', record.tnf)
+print('  type:   ', record.type)
+print('  id:     ', record.id)
+print('  payload:', record.payload)
 
-print ''
-print 'parsing invalid ndef'
+print('')
+print('parsing invalid ndef')
 
-message_data = '9901050155610123456761'.decode('hex')
+message_data = bytes.fromhex('9901050155610123456761')
 try:
     message = ndef.NdefMessage(message_data)
     assert False, 'this should never happen'
-except ndef.InvalidNdef, e:
-    print '  %s: %s' % (e.__class__.__name__, e.message)
+except ndef.InvalidNdef as e:
+    print('  %s: %s' % (e.__class__.__name__, e))
